8f6b8828b244216f2c3d27538722fb176b64f962
David Oberhollenzer <david.oberhollenzer@tele2.at>
2015-04-28 13:28:06 +0200

    Fix bind address, test response to UDP broadcast
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- tests/test_udpbroadcast.c
+++ tests/test_udpbroadcast.c
@@ -27,23 +27,34 @@ int main( void )
     if( !server )
         return EXIT_FAILURE;
 
+    addr.port = 15010;
+    pserver = tl_network_create_packet_server( &addr, TL_ALLOW_BROADCAST );
+    if( !pserver )
+        return EXIT_FAILURE;
+
     /* send broadcast */
     if( !tl_network_get_special_address( &addr, TL_BROADCAST, TL_IPV4 ) )
         return EXIT_FAILURE;
     addr.transport = TL_UDP;
     addr.port = 15000;
-    pserver = tl_network_create_packet_server( &addr, TL_ALLOW_BROADCAST );
-
     if( pserver->send( pserver, "Test", &addr, 4, &val )!=0 || val!=4 )
         return EXIT_FAILURE;
 
-    /* receive broadcast */
+    /* receive broadcast and send response */
     str = server->wait_for_client( server, 5000 );
     if( !str )
         return EXIT_FAILURE;
     if( str->read( str, buffer, sizeof(buffer), &val )!=0 || val!=4 )
         return EXIT_FAILURE;
     if( strncmp( buffer, "Test", 4 )!=0 )
+        return EXIT_FAILURE;
+    if( str->write( str, "Hello", 5, &val )!=0 || val!=5 )
+        return EXIT_FAILURE;
+
+    /* receive respones */
+    if( pserver->receive( pserver, buffer, &addr, 5, &val )!=0 || val!=5 )
+        return EXIT_FAILURE;
+    if( strncmp( buffer, "Hello", 5 )!=0 )
         return EXIT_FAILURE;
 
     /* cleanup */
