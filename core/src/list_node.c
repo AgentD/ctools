ca82f29f15887b0de4309bd75df9a2863afe3366
David Oberhollenzer <david.oberhollenzer@tele2.at>
2016-08-01 18:47:07 +0200

    [cleanup] Split independend list-node code from list
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

---
+++ core/src/list_node.c
@@ -0,0 +1,74 @@
+/* list_node.c -- This file is part of ctools
+ *
+ * Copyright (C) 2015 - David Oberhollenzer
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+#define TL_EXPORT
+#include "tl_allocator.h"
+#include "tl_list.h"
+
+#include <stdlib.h>
+
+#ifdef TL_ALLIGN_MEMORY
+    #define PADDING sizeof(void*)
+    #define ALLIGN( ptr )\
+            if( ((size_t)(ptr)) % PADDING )\
+                (ptr) += PADDING - (((size_t)(ptr)) % PADDING)
+#else
+    #define PADDING 0
+    #define ALLIGN( ptr )
+#endif
+
+
+tl_list_node* tl_list_node_create( const tl_list* this, const void* data )
+{
+    tl_list_node* node;
+    char* ptr;
+
+    assert( this && data );
+
+    node = malloc( sizeof(tl_list_node) + this->unitsize + PADDING );
+
+    ptr = (char*)node + sizeof(tl_list_node);
+    ALLIGN( ptr );
+
+    if( data )
+    {
+        tl_allocator_copy( this->alloc, ptr, data, this->unitsize, 1 );
+    }
+    else
+    {
+        tl_allocator_init( this->alloc, ptr, this->unitsize, 1 );
+    }
+
+    node->prev = node->next = NULL;
+    return node;
+}
+
+void tl_list_node_destroy( tl_list_node* node, tl_list* list )
+{
+    char* ptr = NULL;
+
+    assert( node && list );
+
+    ptr = (char*)node + sizeof(tl_list_node);
+    ALLIGN( ptr );
+
+    tl_allocator_cleanup( list->alloc, ptr, list->unitsize, 1 );
+    free( node );
+}
+
+void* tl_list_node_get_data( const tl_list_node* node )
+{
+    char* ptr = NULL;
+
+    assert( node );
+
+    ptr = (char*)node + sizeof(tl_list_node);
+    ALLIGN( ptr );
+
+    return ptr;
+}
+
