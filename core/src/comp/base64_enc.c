4f169db21b340c70ad4e0d58cab7cef79d6bde9a
David Oberhollenzer <david.oberhollenzer@tele2.at>
2017-10-19 17:53:00 +0200

    Cleanup base64 encoder, fix alligned EOF problem
     - Use inline helper for splitting sextuples & encoding
     - Correctly handle case where flush is set and the input
       is a multiple of 3
    
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- core/src/comp/base64_enc.c
+++ core/src/comp/base64_enc.c
@@ -51,57 +51,57 @@ static void base64_destroy(tl_iostream *stream)
 	free(this);
 }
 
+static TL_INLINE void encode(tl_base64_encoder *this, char *buffer, tl_u32 x)
+{
+	buffer[0] = this->charset[ x        & 0xFF];
+	buffer[1] = this->charset[(x >>  8) & 0xFF];
+	buffer[2] = this->charset[(x >> 16) & 0xFF];
+	buffer[3] = this->charset[(x >> 24) & 0xFF];
+}
+
+static TL_INLINE tl_u32 split(const tl_u8 *ptr, int count)
+{
+	tl_u32 x = ((ptr[0] & 0xFC) >> 2) | ((ptr[0] & 0x03) << 12);
+
+	if (count <= 1)
+		return x | (64 << 16) | (64 << 24);
+
+	x |= ((ptr[1] & 0xF0) << 4) | ((ptr[1] & 0x0F) << 18);
+
+	if (count <= 2)
+		return x | (64 << 24);
+
+	return x | ((ptr[2] & 0xC0) << 10) | ((ptr[2] & 0x3F) << 24);
+}
+
 static int base64_read(base_compressor *super, void *buffer,
 		       size_t size, size_t *actual)
 {
 	tl_base64_encoder *this = (tl_base64_encoder *)super;
-	int ret = 0, total = 0, have, a, b, c, d;
-	const unsigned char *ptr;
+	int ret = 0, total = 0, have;
+	const tl_u8 *ptr;
 
-	ptr = super->buffer;
+	ptr = (const tl_u8 *)super->buffer;
 	have = super->used;
 
 	while (have >= 3 && size >= 4) {
-		a = (ptr[0] >> 2) & 0x3F;
-		b = ((ptr[0] << 4) & 0x30) | ((ptr[1] >> 4) & 0x0F);
-		c = ((ptr[1] << 2) & 0x3C) | ((ptr[2] >> 6) & 0x03);
-		d = ptr[2] & 0x3F;
+		encode(this, buffer, split(ptr, 3));
 		ptr += 3;
 		have -= 3;
-
-		((char *)buffer)[0] = this->charset[a];
-		((char *)buffer)[1] = this->charset[b];
-		((char *)buffer)[2] = this->charset[c];
-		((char *)buffer)[3] = this->charset[d];
 		buffer = (char *)buffer + 4;
 		size -= 4;
 		total += 4;
 	}
 
-	if (have <= 4 && this->flush && size >= 4) {
-		a = (ptr[0] >> 2) & 0x3F;
-		b = (ptr[0] << 4) & 0x30;
-		c = 64;
-		d = 64;
-
-		if (have > 1) {
-			b |= (ptr[1] >> 4) & 0x0F;
-			c = (ptr[1] << 2) & 0x3C;
-
-			if (have > 2) {
-				c |= (ptr[2] >> 6) & 0x03;
-				d = ptr[2] & 0x3F;
-			}
+	if (this->flush && have < 3) {
+		if (have <= 0) {
+			super->eof = 1;
+		} else if (size >= 4) {
+			encode(this, buffer, split(ptr, have));
+			total += 4;
+			super->eof = 1;
+			have = 0;
 		}
-
-		((char *)buffer)[0] = this->charset[a];
-		((char *)buffer)[1] = this->charset[b];
-		((char *)buffer)[2] = this->charset[c];
-		((char *)buffer)[3] = this->charset[d];
-
-		total += 4;
-		super->eof = 1;
-		have = 0;
 	}
 
 	base_compressor_remove(super, super->used - have);
