8cccbed8f67014a4a439bfa5be1b75af25e99061
David Oberhollenzer <david.oberhollenzer@tele2.at>
2017-10-20 03:21:06 +0200

    Additional manual coding style cleanup for rbtree
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- core/src/rbtree.c
+++ core/src/rbtree.c
@@ -24,27 +24,33 @@
 #define ALLIGN( ptr )
 #endif
 
-static void node_recursive_delete(tl_rbtree_node * this, const tl_rbtree * tree)
+static void node_recursive_delete(tl_rbtree_node *this, const tl_rbtree *tree)
 {
+	tl_rbtree_node *l, *r;
 	char *ptr;
+recursion:
+	if (!this)
+		return;
 
-	if (this) {
-		node_recursive_delete(this->left, tree);
-		node_recursive_delete(this->right, tree);
+	l = this->left;
+	r = this->right;
 
-		ptr = (char *)this + sizeof(tl_rbtree_node);
-		ALLIGN(ptr);
-		tl_allocator_cleanup(tree->keyalloc, ptr, tree->keysize, 1);
+	ptr = (char *)this + sizeof(*this);
+	ALLIGN(ptr);
+	tl_allocator_cleanup(tree->keyalloc, ptr, tree->keysize, 1);
 
-		ptr += tree->keysize;
-		ALLIGN(ptr);
-		tl_allocator_cleanup(tree->valalloc, ptr, tree->valuesize, 1);
+	ptr += tree->keysize;
+	ALLIGN(ptr);
+	tl_allocator_cleanup(tree->valalloc, ptr, tree->valuesize, 1);
 
-		free(this);
-	}
+	free(this);
+
+	node_recursive_delete(l, tree);
+	this = r;
+	goto recursion;
 }
 
-static void flip_colors(tl_rbtree_node * this)
+static void flip_colors(tl_rbtree_node *this)
 {
 	this->is_red = !this->is_red;
 	this->left->is_red = !this->left->is_red;
@@ -64,7 +70,7 @@ static tl_rbtree_node *rotate_right(tl_rbtree_node * this)
 	return x;
 }
 
-static tl_rbtree_node *rotate_left(tl_rbtree_node * this)
+static tl_rbtree_node *rotate_left(tl_rbtree_node *this)
 {
 	tl_rbtree_node *x;
 
@@ -77,7 +83,7 @@ static tl_rbtree_node *rotate_left(tl_rbtree_node * this)
 	return x;
 }
 
-static tl_rbtree_node *subtree_balance(tl_rbtree_node * this)
+static tl_rbtree_node *subtree_balance(tl_rbtree_node *this)
 {
 	if (IS_RED(this->right) && !IS_RED(this->left))
 		this = rotate_left(this);
@@ -91,7 +97,7 @@ static tl_rbtree_node *subtree_balance(tl_rbtree_node * this)
 	return this;
 }
 
-static tl_rbtree_node *move_red_left(tl_rbtree_node * this)
+static tl_rbtree_node *move_red_left(tl_rbtree_node *this)
 {
 	flip_colors(this);
 
@@ -104,19 +110,17 @@ static tl_rbtree_node *move_red_left(tl_rbtree_node * this)
 	return this;
 }
 
-static tl_rbtree_node *move_red_right(tl_rbtree_node * this)
+static tl_rbtree_node *move_red_right(tl_rbtree_node *this)
 {
 	flip_colors(this);
 
 	return IS_RED(this->left->left) ? rotate_right(this) : this;
 }
 
-static tl_rbtree_node *subtree_insert(tl_rbtree * this,
-				      tl_rbtree_node * root,
-				      tl_rbtree_node * newnode)
+static tl_rbtree_node *subtree_insert(tl_rbtree *this, tl_rbtree_node *root,
+				      tl_rbtree_node *newnode)
 {
-	void *newkey;
-	void *key;
+	void *newkey, *key;
 
 	if (!root)
 		return newnode;
@@ -133,13 +137,13 @@ static tl_rbtree_node *subtree_insert(tl_rbtree * this,
 	return subtree_balance(root);
 }
 
-static tl_rbtree_node *remove_min_from_subtree(tl_rbtree_node * this,
-					       tl_rbtree * tree)
+static tl_rbtree_node *remove_min_from_subtree(tl_rbtree_node *this,
+					       tl_rbtree *tree)
 {
 	char *ptr;
 
 	if (!this->left) {
-		ptr = (char *)this + sizeof(tl_rbtree_node);
+		ptr = (char *)this + sizeof(*this);
 		ALLIGN(ptr);
 		tl_allocator_cleanup(tree->keyalloc, ptr, tree->keysize, 1);
 
@@ -158,8 +162,8 @@ static tl_rbtree_node *remove_min_from_subtree(tl_rbtree_node * this,
 	return subtree_balance(this);
 }
 
-static tl_rbtree_node *remove_max_from_subtree(tl_rbtree_node * this,
-					       tl_rbtree * tree)
+static tl_rbtree_node *remove_max_from_subtree(tl_rbtree_node *this,
+					       tl_rbtree *tree)
 {
 	char *ptr;
 
@@ -167,7 +171,7 @@ static tl_rbtree_node *remove_max_from_subtree(tl_rbtree_node * this,
 		this = rotate_right(this);
 
 	if (!this->right) {
-		ptr = (char *)this + sizeof(tl_rbtree_node);
+		ptr = (char *)this + sizeof(*this);
 		ALLIGN(ptr);
 		tl_allocator_cleanup(tree->keyalloc, ptr, tree->keysize, 1);
 
@@ -186,8 +190,8 @@ static tl_rbtree_node *remove_max_from_subtree(tl_rbtree_node * this,
 	return subtree_balance(this);
 }
 
-static tl_rbtree_node *remove_from_subtree(tl_rbtree * this,
-					   tl_rbtree_node * root,
+static tl_rbtree_node *remove_from_subtree(tl_rbtree *this,
+					   tl_rbtree_node *root,
 					   const void *key)
 {
 	tl_rbtree_node *min;
@@ -201,7 +205,7 @@ static tl_rbtree_node *remove_from_subtree(tl_rbtree * this,
 		if (IS_RED(root->left))
 			root = rotate_right(root);
 
-		if (this->compare(key, tl_rbtree_node_get_key(this, root)) == 0
+		if (!this->compare(key, tl_rbtree_node_get_key(this, root))
 		    && !(root->right)) {
 			free(root);
 			return NULL;
@@ -210,14 +214,14 @@ static tl_rbtree_node *remove_from_subtree(tl_rbtree * this,
 		if (!IS_RED(root->right) && !IS_RED(root->right->left))
 			root = move_red_right(root);
 
-		if (this->compare(key, tl_rbtree_node_get_key(this, root)) == 0) {
+		if (!this->compare(key, tl_rbtree_node_get_key(this, root))) {
 			/* find minimum of right subtree */
-			for (min = root->right; min->left; min = min->left) {
-			}
+			for (min = root->right; min->left; min = min->left)
+				;
 
 			/* "swap" minimum of right subtree with root */
-			memcpy((unsigned char *)root + sizeof(tl_rbtree_node),
-			       (unsigned char *)min + sizeof(tl_rbtree_node),
+			memcpy((unsigned char *)root + sizeof(*root),
+			       (unsigned char *)min + sizeof(*min),
 			       2 * PADDING + this->keysize + this->valuesize);
 
 			/* remove minimum of the right subtree */
@@ -234,7 +238,7 @@ static tl_rbtree_node *remove_from_subtree(tl_rbtree * this,
 
 /****************************************************************************/
 
-tl_rbtree_node *tl_rbtree_node_create(const tl_rbtree * tree,
+tl_rbtree_node *tl_rbtree_node_create(const tl_rbtree *tree,
 				      const void *key, const void *value)
 {
 	tl_rbtree_node *node;
@@ -242,40 +246,39 @@ tl_rbtree_node *tl_rbtree_node_create(const tl_rbtree * tree,
 
 	assert(tree);
 
-	node = malloc(sizeof(tl_rbtree_node) + 2 * PADDING +
-		      tree->keysize + tree->valuesize);
+	node = calloc(1, sizeof(*node) + 2 * PADDING +
+			 tree->keysize + tree->valuesize);
 
 	if (!node)
 		return NULL;
 
-	node->left = NULL;
-	node->right = NULL;
 	node->is_red = 1;
 
 	/* set key pointer */
 	ptr = (unsigned char *)node + sizeof(tl_rbtree_node);
 	ALLIGN(ptr);
 
-	if (key)
+	if (key) {
 		tl_allocator_copy(tree->keyalloc, ptr, key, tree->keysize, 1);
-	else
+	} else {
 		tl_allocator_init(tree->keyalloc, ptr, tree->keysize, 1);
+	}
 
 	/* set value */
 	ptr += tree->keysize;
 	ALLIGN(ptr);
 
-	if (value)
+	if (value) {
 		tl_allocator_copy(tree->valalloc, ptr, value, tree->valuesize,
 				  1);
-	else
+	} else {
 		tl_allocator_init(tree->valalloc, ptr, tree->valuesize, 1);
-
+	}
 	return node;
 }
 
-void *tl_rbtree_node_get_key(const tl_rbtree * tree,
-			     const tl_rbtree_node * node)
+void *tl_rbtree_node_get_key(const tl_rbtree *tree,
+			     const tl_rbtree_node *node)
 {
 	char *ptr;
 	(void)tree;
@@ -288,8 +291,8 @@ void *tl_rbtree_node_get_key(const tl_rbtree * tree,
 	return ptr;
 }
 
-void *tl_rbtree_node_get_value(const tl_rbtree * tree,
-			       const tl_rbtree_node * node)
+void *tl_rbtree_node_get_value(const tl_rbtree *tree,
+			       const tl_rbtree_node *node)
 {
 	char *ptr;
 
@@ -304,39 +307,37 @@ void *tl_rbtree_node_get_value(const tl_rbtree * tree,
 	return ptr;
 }
 
-static tl_rbtree_node *copy_subtree(const tl_rbtree * this,
-				    const tl_rbtree_node * src)
+static tl_rbtree_node *copy_subtree(const tl_rbtree *this,
+				    const tl_rbtree_node *src)
 {
 	tl_rbtree_node *copy;
-	char *value;
-	char *key;
+	char *value, *key;
 
 	if (!src)
 		return NULL;
 
-	/* create a copy node */
 	key = tl_rbtree_node_get_key(this, src);
 	value = tl_rbtree_node_get_value(this, src);
 	copy = tl_rbtree_node_create(this, key, value);
 
-	/* copy subtrees */
-	if (copy) {
-		copy->left = copy_subtree(this, src->left);
-		copy->right = copy_subtree(this, src->right);
+	if (!copy)
+		return NULL;
 
-		if ((src->left && !copy->left) || (src->right && !copy->right)) {
-			node_recursive_delete(copy, this);
-			copy = NULL;
-		}
+	copy->left = copy_subtree(this, src->left);
+	copy->right = copy_subtree(this, src->right);
+
+	if ((src->left && !copy->left) || (src->right && !copy->right)) {
+		node_recursive_delete(copy, this);
+		copy = NULL;
 	}
 	return copy;
 }
 
 /****************************************************************************/
 
-void tl_rbtree_init(tl_rbtree * this, size_t keysize, size_t valuesize,
-		    tl_compare comparefun, tl_allocator * keyalloc,
-		    tl_allocator * valalloc)
+void tl_rbtree_init(tl_rbtree *this, size_t keysize, size_t valuesize,
+		    tl_compare comparefun, tl_allocator *keyalloc,
+		    tl_allocator *valalloc)
 {
 	assert(this);
 
@@ -349,7 +350,7 @@ void tl_rbtree_init(tl_rbtree * this, size_t keysize, size_t valuesize,
 	this->valalloc = valalloc;
 }
 
-void tl_rbtree_cleanup(tl_rbtree * this)
+void tl_rbtree_cleanup(tl_rbtree *this)
 {
 	assert(this);
 
@@ -358,19 +359,17 @@ void tl_rbtree_cleanup(tl_rbtree * this)
 	this->size = 0;
 }
 
-int tl_rbtree_copy(tl_rbtree * this, const tl_rbtree * src)
+int tl_rbtree_copy(tl_rbtree *this, const tl_rbtree *src)
 {
 	tl_rbtree_node *newroot;
 
 	assert(this && src);
 
-	/* create a copy of the source tree */
 	newroot = copy_subtree(src, src->root);
 
 	if (!newroot)
 		return 0;
 
-	/* copy over new tree and data of source tree */
 	node_recursive_delete(this->root, this);
 	this->root = newroot;
 	this->size = src->size;
@@ -382,7 +381,7 @@ int tl_rbtree_copy(tl_rbtree * this, const tl_rbtree * src)
 	return 1;
 }
 
-int tl_rbtree_insert(tl_rbtree * this, const void *key, const void *value)
+int tl_rbtree_insert(tl_rbtree *this, const void *key, const void *value)
 {
 	tl_rbtree_node *node;
 
@@ -400,31 +399,28 @@ int tl_rbtree_insert(tl_rbtree * this, const void *key, const void *value)
 	return 1;
 }
 
-void *tl_rbtree_at(const tl_rbtree * this, const void *key)
+void *tl_rbtree_at(const tl_rbtree *this, const void *key)
 {
 	tl_rbtree_node *node;
 	void *nodekey;
+	int ret;
 
 	assert(this && key);
 
-	node = this->root;
-
-	while (node) {
+	for (node = this->root; node != NULL; ) {
 		nodekey = tl_rbtree_node_get_key(this, node);
+		ret = this->compare(key, nodekey);
 
-		if (this->compare(key, nodekey) == 0) {
+		if (ret == 0)
 			return tl_rbtree_node_get_value(this, node);
-		}
 
-		node =
-		    (this->compare(key, nodekey) <
-		     0) ? node->left : node->right;
+		node = ret < 0 ? node->left : node->right;
 	}
 
 	return NULL;
 }
 
-int tl_rbtree_set(tl_rbtree * this, const void *key, const void *value)
+int tl_rbtree_set(tl_rbtree *this, const void *key, const void *value)
 {
 	void *ptr;
 
@@ -438,20 +434,17 @@ int tl_rbtree_set(tl_rbtree * this, const void *key, const void *value)
 	return 1;
 }
 
-int tl_rbtree_get_min(const tl_rbtree * this, void **key, void **value)
+int tl_rbtree_get_min(const tl_rbtree *this, void **key, void **value)
 {
 	tl_rbtree_node *n;
 
 	assert(this);
 
-	if (!this->root)
+	for (n = this->root; n->left; n = n->left)
+		;
+
+	if (!n)
 		return 0;
-
-	if (!key && !value)
-		return 1;
-
-	for (n = this->root; n->left; n = n->left) {
-	}
 
 	if (key)
 		*key = tl_rbtree_node_get_key(this, n);
@@ -462,20 +455,17 @@ int tl_rbtree_get_min(const tl_rbtree * this, void **key, void **value)
 	return 1;
 }
 
-int tl_rbtree_get_max(const tl_rbtree * this, void **key, void **value)
+int tl_rbtree_get_max(const tl_rbtree *this, void **key, void **value)
 {
 	tl_rbtree_node *n;
 
 	assert(this);
 
-	if (!this->root)
+	for (n = this->root; n->right; n = n->right)
+		;
+
+	if (!n)
 		return 0;
-
-	if (!key && !value)
-		return 1;
-
-	for (n = this->root; n->right; n = n->right) {
-	}
 
 	if (key)
 		*key = tl_rbtree_node_get_key(this, n);
@@ -486,7 +476,7 @@ int tl_rbtree_get_max(const tl_rbtree * this, void **key, void **value)
 	return 1;
 }
 
-void tl_rbtree_remove_min(tl_rbtree * this)
+void tl_rbtree_remove_min(tl_rbtree *this)
 {
 	assert(this);
 
@@ -505,7 +495,7 @@ void tl_rbtree_remove_min(tl_rbtree * this)
 	--(this->size);
 }
 
-void tl_rbtree_remove_max(tl_rbtree * this)
+void tl_rbtree_remove_max(tl_rbtree *this)
 {
 	assert(this);
 
@@ -524,27 +514,24 @@ void tl_rbtree_remove_max(tl_rbtree * this)
 	--(this->size);
 }
 
-int tl_rbtree_remove(tl_rbtree * this, const void *key, void *value)
+int tl_rbtree_remove(tl_rbtree *this, const void *key, void *value)
 {
 
 	tl_rbtree_node *node;
 	char *ptr;
+	int ret;
 
 	assert(this && key);
 
-	if (!this->size)
-		return 0;
-
 	/* find node */
-	node = this->root;
-
-	while (node) {
+	for (node = this->root; node != NULL; ) {
 		ptr = tl_rbtree_node_get_key(this, node);
+		ret = this->compare(key, ptr);
 
-		if (this->compare(key, ptr) == 0)
+		if (ret == 0)
 			break;
 
-		node = (this->compare(key, ptr) < 0) ? node->left : node->right;
+		node = ret < 0 ? node->left : node->right;
 	}
 
 	if (!node)
@@ -555,10 +542,11 @@ int tl_rbtree_remove(tl_rbtree * this, const void *key, void *value)
 	ptr += this->keysize;
 	ALLIGN(ptr);
 
-	if (value)
+	if (value) {
 		memcpy(value, ptr, this->valuesize);
-	else
+	} else {
 		tl_allocator_cleanup(this->valalloc, ptr, this->valuesize, 1);
+	}
 
 	/* remove node */
 	this->root = remove_from_subtree(this, this->root, key);
@@ -570,7 +558,7 @@ int tl_rbtree_remove(tl_rbtree * this, const void *key, void *value)
 	return 1;
 }
 
-void tl_rbtree_clear(tl_rbtree * this)
+void tl_rbtree_clear(tl_rbtree *this)
 {
 	assert(this);
 
